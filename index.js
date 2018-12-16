75261148ecd66d799155c47b6ac9ecf1bd1faa87
Shaikh <samin@teamamericany.com>
2018-12-17 01:28:29 +0600

    redis node hashmap docs

---
+++ index.js
@@ -0,0 +1,39 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.get('/', function(req, res){
+    //res.send('<h1>Hello world</h1>');
+    res.status(200);
+});
+
+
+io.use((socket, next) => {
+    let token = socket.handshake.query.token;
+    if(token === '123456'){
+        return next();
+    }
+        
+    return next(new Error('authentication error'));
+}); 
+
+io.on('connection', (socket) => {
+
+    socket.emit('connected', 'You are connected message will be send with user infornation by the token');
+    console.log('token is:'+ socket.handshake.query.token);
+       
+    //console.log('connection established by :'+socket);
+    socket.on('emit-chat',(msg)=>{
+        console.log('message recieved from client :'+msg);
+        //broadcasting all user except sender
+        socket.broadcast.emit('broadcast', msg);
+
+    });
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+server.listen(3000,()=>{
+    console.log('Server started on :'+3000);
+});
---
+++ redis-node.md
@@ -0,0 +1,28 @@
+#https://redis.io/commands/hdel
+You can use same hmset/hset based on how many you want to add or update
+
+var redis = require('redis');
+var client = redis.createClient(); //creates a new client
+
+client.on('connect', function() {
+    console.log('connected');
+});
+
+client.hmset('frameworks', {
+    'javascript': 'AngularJS',
+    'css': 'Bootstrap',
+    'node': 'Express'
+});
+Say you initially have this and want to add db : mongo, and want to update node: Express4 then you can just use
+
+//If you know will update only one use hset instead
+client.hmset('frameworks', {
+    'node': 'Express4',
+    'db' : 'MongoDB'
+});
+
+
+//delete hash map 
+#https://redis.io/commands/hdel
+
+client.hdel('offer', id)
